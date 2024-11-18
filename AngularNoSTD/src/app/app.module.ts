fe97227aef485e64ec9fb96f459c132d5baee88f
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-11-18 17:07:25 +0100

    added login and register

--- AngularNoSTD/src/app/app.module.ts
+++ AngularNoSTD/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
