2a41be5054c0a404270c5d8e3eab7927916fe394
Medamine-bahassou <mohamedamine.bahassou@gmail.com>
2024-11-24 00:40:36 +0100

    new style dashboard

--- AngularNoSTD/tailwind.config.js
+++ AngularNoSTD/tailwind.config.js
@@ -4,13 +4,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-<<<<<<< HEAD
   plugins: [require('daisyui'),],
   daisyui: {
     themes: ["light", "dark"],
   },
-=======
-  plugins: [require('daisyui')],
->>>>>>> f418a70012cfbbd2a488db11ebc2f5990a0f33ce
 }
 
