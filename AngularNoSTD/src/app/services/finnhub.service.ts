d78893a4174556854f71909ec83c55c70332f122
Medamine-bahassou <mohamedamine.bahassou@gmail.com>
2024-11-24 02:07:56 +0100

    3 - apexchart, real time news (finnhub api)

---
+++ AngularNoSTD/src/app/services/finnhub.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinnhubService {
+
+  private apiKey = 'cssart9r01qld5m1bar0cssart9r01qld5m1barg'; // Replace with your Finnhub API key
+  private baseUrl = 'https://finnhub.io/api/v1';
+
+  constructor(private http: HttpClient) {}
+
+  getCompanyNews(symbol: string, from: string, to: string): Observable<any> {
+    const params = new HttpParams()
+      .set('symbol', symbol)
+      .set('from', from)
+      .set('to', to)
+      .set('token', this.apiKey); // Use your Finnhub API key
+  
+    return this.http.get(`${this.baseUrl}/company-news`, { params });
+  }
+}
